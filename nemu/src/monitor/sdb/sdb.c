2968f6ad1d041b5237aeff8c5ce64e915e8f1619
231180181-An Runsen <2634032821@qq.com>
2026-04-09 21:06:40 +0800

    Finish the dubuger

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,6 +18,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
+#include <memory/paddr.h>
 
 static int is_batch_mode = false;
 
@@ -57,6 +58,8 @@ static int cmd_si(char *args) {
   return 0;
 }
 
+static int cmd_x(char *args);
+
 static int cmd_help(char *args);
 static int cmd_info(char *args);
 
@@ -70,6 +73,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
   { "si", "Step in one instruction", cmd_si},
   { "info", "Print the info of reg or the watch point", cmd_info},
+  { "x", "Scan the memory", cmd_x}
 
   /* TODO: Add more commands */
 
@@ -106,7 +110,7 @@ static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
 
   if (arg == NULL) {
-    printf("Hint: Try <info r> or <info w>");
+    printf("Hint: Try <info r> or <info w>\n");
   }
 
   else if(strcmp(arg, "r") == 0) {
@@ -114,10 +118,37 @@ static int cmd_info(char *args) {
   }
 
   else if(strcmp(arg, "w") == 0) {
-    printf("Impl me in sdb.c:116");
+    printf("Impl me in sdb.c:116\n");
   }
   else {
     printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+static int cmd_x(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+
+  if (arg == NULL) {
+    printf("Hint: Try x <num> <addr>\n");
+  }
+
+  else{
+    int scannum = atoi(arg);
+
+    arg = strtok(NULL, " ");
+    if(arg == NULL) {
+      printf("Hint: Try x <num> <addr>\n");
+    }
+    else {
+      int startaddr = (int)strtol(arg, NULL, 0);
+      for(int i=0; i<scannum; i++)
+      {
+        printf("0x%08x: 0x%08x\n", startaddr + i*4, paddr_read(startaddr + i*4, 4));
+      }
+    }
+    
   }
   return 0;
 }
