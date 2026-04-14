f06875a55f52a82acd5851c41bbb1acbcea3c0f9
tracer-ysyx <tracer@ysyx.org>
2026-04-14 18:43:26 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:43:26 up  5:45,  3 users,  load average: 0.81, 0.79, 0.86

--- npc/csrc/CPU.cpp
+++ npc/csrc/CPU.cpp
@@ -44,7 +44,7 @@ void load_bin(const char *path) {
 int main()
 {
     load_bin("./logisim-bin/sum.bin");
-    *(uint32_t*)(mem+0x224) = 0x00100073;
+    // *(uint32_t*)(mem+0x224) = 0x00100073;
 
     Verilated::traceEverOn(true);
     VCPU* cpu = new VCPU;
