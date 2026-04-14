676c627c7f66ba58e807a8b4b454031afad930c7
tracer-ysyx <tracer@ysyx.org>
2026-04-14 18:47:40 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:47:40 up  5:49,  3 users,  load average: 0.41, 0.63, 0.78

--- npc/csrc/CPU.cpp
+++ npc/csrc/CPU.cpp
@@ -13,11 +13,13 @@ extern "C" void sim_exit() {
 
 extern "C" int pmem_read(int raddr)
 {
+    printf("Read from address 0x%08x\n", raddr);
     // 总是读取地址为`raddr & ~0x3u`的4字节返回
     return *(uint32_t*)(mem + (raddr & ~0x3u));
 }
 extern "C" void pmem_write(int waddr, int wdata, uint8_t wmask)
 {
+    printf("Write to address 0x%08x, data 0x%08x, wmask 0x%02x\n", waddr, wdata, wmask);
     // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
     // `wmask`中每比特表示`wdata`中1个字节的掩码,
     // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
