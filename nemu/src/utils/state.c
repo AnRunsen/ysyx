5bacc0f3fda82ebab8e09c0d586f768802b73da5
tracer-ysyx <tracer@ysyx.org>
2026-04-15 10:43:21 +0800

    > compile NEMU ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:43:21 up 47 min,  1 user,  load average: 0.48, 0.53, 0.57

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -28,7 +28,7 @@ int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
 
-  if(!good)
+  if(good)
   {
     //将环形缓冲区中的内容输出到屏幕
     do{
