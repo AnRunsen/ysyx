b30f7b185fb70f9c07df5a1497551330e8a4f566
tracer-ysyx <tracer@ysyx.org>
2026-04-16 18:15:34 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:15:34 up  4:30,  3 users,  load average: 0.58, 0.46, 0.26

--- npc/csrc/CPU.cpp
+++ npc/csrc/CPU.cpp
@@ -29,8 +29,14 @@ extern "C" void pmem_write(int waddr, int wdata, uint8_t wmask)
     // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
     // `wmask`中每比特表示`wdata`中1个字节的掩码,
     // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-    waddr = waddr - 0x80000000; // 内存映射地址转换
+    
+    if(waddr == 0x10000000){
+        // 0x10000000是一个特殊的地址, 往这里写入数据会打印到控制台
+        putchar(wdata & 0xFF);
+        return;
+    }
 
+    waddr = waddr - 0x80000000; // 内存映射地址转换
     for(int i = 0; i < 4; i++){
         if(wmask & (1 << i)){
             mem[(waddr & ~0x3u) + i] = (wdata >> (i << 3)) & 0xFF;
@@ -59,7 +65,6 @@ int main(int argc, char **argv)
         printf("please provide a binary file to load into memory\n");
         assert(0);
     }
-    //*(uint32_t*)(mem+0x224) = 0x00100073;
 
     Verilated::traceEverOn(true);
     VCPU* cpu = new VCPU;
