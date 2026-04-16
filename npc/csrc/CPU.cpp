2972c9537d50be234e6e2fabd0a105198c502627
tracer-ysyx <tracer@ysyx.org>
2026-04-16 21:51:03 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:51:03 up  8:06,  4 users,  load average: 0.57, 0.77, 0.76

--- npc/csrc/CPU.cpp
+++ npc/csrc/CPU.cpp
@@ -10,68 +10,6 @@ uint8_t mem[0x8000000]; // 128MB memory
 bool exit_flag = false;
 
 
-extern "C" void sim_exit(int code) {
-    if(code == 0){
-        printf("Code:%d \033[32;1mHit Good Trap\033[0m\n", code);
-    }
-    else if(code == 1){
-        printf("Code:%d \033[31;1mHit Bad Trap\033[0m\n", code);
-    }
-
-    else {
-        printf("\033[31;1mUnknown Opcode:%08x\033[0m\n", code);
-    }
-    exit_flag = true;
-}
-
-extern "C" int pmem_read(int raddr)
-{
-#ifdef DEBUG
-    printf("Read: addr=0x%08x\n", raddr);
-#endif
-    //串口相关
-    if(raddr == 0x10000000){
-        return 0;
-    }
-
-    //RTC相关
-    else if(raddr == 0x10000004 || raddr == 0x10000008){
-        if(raddr == 0x10000004){
-            // 0x10000004是一个特殊的地址, 读取这里会返回当前时间的低32位
-            return (uint32_t)(clock());
-        } else {
-            // 0x10000008是一个特殊的地址, 读取这里会返回当前时间的高32位
-            return (uint32_t)(clock() >> 32);
-        }
-    }
-
-    raddr = raddr - 0x80000000; // 内存映射地址转换
-    // 总是读取地址为`raddr & ~0x3u`的4字节返回
-    return *(uint32_t*)(mem + (raddr & ~0x3u));
-}
-extern "C" void pmem_write(int waddr, int wdata, uint8_t wmask)
-{
-#ifdef DEBUG
-    printf("Write: addr=0x%08x, data=0x%08x, wmask=0x%02x\n", waddr, wdata, wmask);
-#endif
-    // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
-    // `wmask`中每比特表示`wdata`中1个字节的掩码,
-    // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-    if(waddr == 0x10000000){
-        // 0x10000000是一个特殊的地址, 往这里写入数据会打印到控制台
-        putchar(wdata & 0xFF);
-        return;
-    }
-
-    waddr = waddr - 0x80000000; // 内存映射地址转换
-    for(int i = 0; i < 4; i++){
-        if(wmask & (1 << i)){
-            mem[(waddr & ~0x3u) + i] = (wdata >> (i << 3)) & 0xFF;
-        }
-    }
-}
-
-
 void load_bin(const char *path) {
     FILE *fp = fopen(path, "rb");
     assert(fp != NULL);
