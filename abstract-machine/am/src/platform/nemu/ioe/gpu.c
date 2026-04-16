9ceda72050d8f3225f49baa4babc160c13ef4e68
tracer-ysyx <tracer@ysyx.org>
2026-04-16 15:20:58 +0800

    > compile NEMU ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:20:58 up  1:36,  3 users,  load average: 0.25, 0.35, 0.46

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,29 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = inw(VGACTL_ADDR + 2);
+  int h = inw(VGACTL_ADDR);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t h = inw(VGACTL_ADDR);
+  uint32_t w = inw(VGACTL_ADDR + 2);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = w, .height = h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  //uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
