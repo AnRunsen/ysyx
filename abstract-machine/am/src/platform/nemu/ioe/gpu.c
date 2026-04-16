bf80a5bf49af46a6468881cf46185307dd5820c2
tracer-ysyx <tracer@ysyx.org>
2026-04-16 15:41:35 +0800

    > compile NEMU ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:41:35 up  1:56,  3 users,  load average: 0.47, 0.53, 0.46

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,16 +23,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int size_w = inw(VGACTL_ADDR + 2);
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // int size_w = inw(VGACTL_ADDR + 2);
 
-  for (int y = 0; y < ctl->h; y ++) {
-    for (int x = 0; x < ctl->w; x ++) {
-      int fb_idx = (ctl->y + y) * size_w + ctl->x + x;
-      int buf_idx = y * ctl->w + x;
-      fb[fb_idx] = ((uint32_t *)ctl->pixels)[buf_idx];
-    }
-  }
+  // for (int y = 0; y < ctl->h; y ++) {
+  //   for (int x = 0; x < ctl->w; x ++) {
+  //     int fb_idx = (ctl->y + y) * size_w + ctl->x + x;
+  //     int buf_idx = y * ctl->w + x;
+  //     fb[fb_idx] = ((uint32_t *)ctl->pixels)[buf_idx];
+  //   }
+  // }
 
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
