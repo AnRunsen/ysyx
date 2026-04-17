eb0b420edbc9ad4227bf611cc15d4b7759e1aa92
tracer-ysyx <tracer@ysyx.org>
2026-04-17 21:50:08 +0800

    > compile NEMU ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:50:08 up  7:01,  3 users,  load average: 0.83, 1.07, 0.91

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -37,14 +37,14 @@ __EXPORT void difftest_memcpy(uint32_t addr, void *buf, size_t n, bool direction
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   if(direction == DIFFTEST_TO_DUT) {
     CPU_state *dut_ = (CPU_state*)dut;
-    for(int i = 0; i < sizeof(cpu.gpr)/sizeof(cpu.gpr[0]); i++) {
+    for(int i = 0; i < 16; i++) {
       dut_->gpr[i] = cpu.gpr[i];
     }
     dut_->pc = cpu.pc;
   }
   else {
     CPU_state *dut_ = (CPU_state*)dut;
-    for(int i = 0; i < sizeof(cpu.gpr)/sizeof(cpu.gpr[0]); i++) {
+    for(int i = 0; i < 16; i++) {
       cpu.gpr[i] = dut_->gpr[i];
     }
     cpu.pc = dut_->pc;
