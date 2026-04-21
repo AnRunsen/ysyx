63b269a43245a9e739c7065e3f824bc0399e840e
tracer-ysyx <tracer@ysyx.org>
2026-04-21 12:37:20 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:37:20 up  2:38,  3 users,  load average: 0.59, 0.58, 0.41

--- npc/vsrc/CSR.v
+++ npc/vsrc/CSR.v
@@ -11,11 +11,15 @@ module CSR(
 
     reg [31:0] mcycle;
     reg [31:0] mcycleh;
+    reg [31:0] mvendorid;
+    reg [31:0] marchid;
 
     //a combinational logic to read CSR, only take the mycle(h) into account
     always @(*) begin
         if(addr == 12'hc00) rdata = mcycle; //mcycle
         else if(addr == 12'hc80) rdata = mcycleh; //mcycleh
+        else if(addr == 12'hf11) rdata = mvendorid; //mvendorid
+        else if(addr == 12'hf12) rdata = marchid; //marchid
         else rdata = 32'b0;
     end
 
@@ -29,5 +33,14 @@ module CSR(
         else if(mcycle == 32'hffff_ffff) mcycleh <= mcycleh + 1;
     end
 
+    always @(posedge clk or negedge arstn) begin
+        if(!arstn) mvendorid <= 32'b0;
+        else mvendorid <= 32'h79737978; //just a random value
+    end
+
+    always @(posedge clk or negedge arstn) begin
+        if(!arstn) marchid <= 32'b0;
+        else marchid <= 32'h0DC78795; //231180181 in hex
+    end
 
 endmodule
