ea7a94dc98f97ccb878ab98033749f40eabf15f3
tracer-ysyx <tracer@ysyx.org>
2026-04-29 15:45:49 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:45:49 up  5:39,  3 users,  load average: 0.42, 0.39, 0.38

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -9,6 +9,10 @@ extern char _pmem_start;
 
 #define UART_BASE 0x10000000L
 #define UART_TX   0x0
+#define UART_LCR  0x3
+#define UART_LSR  0x5
+#define UART_DL_MSB 0x1
+#define UART_DL_LSB 0x0
 
 #define PMEM_SIZE (8 * 1024)// 8KB
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
@@ -20,6 +24,8 @@ Area heap = RANGE(&_heap_start, HEAP_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
+  while(!(*(volatile uint8_t*)(UART_BASE + UART_LSR) & 0x20));
+
   *(volatile uint8_t*)(UART_BASE + UART_TX) = ch;
 }
 
@@ -28,12 +34,29 @@ void halt(int code) {
   while (1);
 }
 
-void _trm_init() {
-  extern char _data_start;
+void load()
+{
+  extern char _sdata;
   extern char _data_lma;
-  extern char edata;
-  memcpy(&_data_start, &_data_lma, &edata - &_data_start);
+  extern char _edata;
+  memcpy(&_sdata, &_data_lma, &_edata - &_sdata);
 
+  extern char _sbss;
+  extern char _ebss;
+  memset(&_sbss, 0, &_ebss - &_sbss);
+}
+
+void uart_init() {
+  *(volatile uint8_t*)(UART_BASE + UART_LCR) |= 0x80;
+  *(volatile uint8_t*)(UART_BASE + UART_DL_MSB) = 0x00;
+  *(volatile uint8_t*)(UART_BASE + UART_DL_LSB) = 0x01;
+  *(volatile uint8_t*)(UART_BASE + UART_LCR) &= 0x7F;
+}
+
+void _trm_init() {
+  load();
+
+  uart_init();
   int ret = main(mainargs);
   halt(ret);
 }
