86ce532fbaede86bfc399de8cc7d7d602dfb9395
tracer-ysyx <tracer@ysyx.org>
2026-05-02 15:05:57 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:05:57 up 17 min,  3 users,  load average: 1.12, 0.69, 0.42

--- npc/csrc/src/DPIC.cpp
+++ npc/csrc/src/DPIC.cpp
@@ -165,5 +165,6 @@ extern "C" void psram_read(uint32_t addr, uint32_t *data) {
     *data = *(uint32_t *)(psram + (addr & ~0x3u));
 }
 extern "C" void psram_write(uint32_t addr, uint8_t data) {
+    printf("PSRAM Write: addr=0x%08x, data=0x%02x\n", addr, data);
     *(psram + addr) = data;
 }
