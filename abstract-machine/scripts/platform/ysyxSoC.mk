b309aa48bbc0fc4f0731a139ecc5fb986b77ed36
tracer-ysyx <tracer@ysyx.org>
2026-05-03 20:45:13 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:45:13 up  1:28,  3 users,  load average: 3.06, 3.13, 2.85

--- abstract-machine/scripts/platform/ysyxSoC.mk
+++ abstract-machine/scripts/platform/ysyxSoC.mk
@@ -1,5 +1,6 @@
 AM_SRCS := riscv/ysyxSoC/start.S \
-           riscv/ysyxSoC/bootloader.S \
+           riscv/ysyxSoC/fsbl.S \
+           riscv/ysyxSoC/ssbl.S \
            riscv/ysyxSoC/trm.c \
            riscv/ysyxSoC/ioe.c \
            riscv/ysyxSoC/timer.c \
@@ -11,7 +12,7 @@ AM_SRCS := riscv/ysyxSoC/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/ysyxSoC.ld
-LDFLAGS   += --gc-sections -e _bootloader
+LDFLAGS   += --gc-sections -e _fsbl
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
