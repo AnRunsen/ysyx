cc6f1bffd94a54a3224a5823d214ebbd5538445d
tracer-ysyx <tracer@ysyx.org>
2026-05-03 20:54:24 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:54:24 up  1:37,  4 users,  load average: 4.39, 3.66, 3.16

--- npc/vsrc/PKG.v
+++ npc/vsrc/PKG.v
@@ -1,9 +1,4 @@
 package PKG;
-
-    import "DPI-C" function int pmem_read(input int raddr);
-    import "DPI-C" function void pmem_write(
-        input int waddr, input int wdata, input byte wmask
-    );
     import "DPI-C" function void sim_exit(input int code);
     import "DPI-C" function void itrace(input int inst, input int pc);
     import "DPI-C" function void ftrace(input int pc, input int npc);
