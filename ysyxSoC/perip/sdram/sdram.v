aa5b705de38ddd28d765c540e49510e43d250a68
tracer-ysyx <tracer@ysyx.org>
2026-05-04 20:26:18 +0800

    > sim RTL ysyx_231180181 安润森 Linux Legion 6.8.0-110-generic #110~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 27 12:43:08 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:26:18 up  8:07,  3 users,  load average: 0.51, 0.52, 0.60

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -22,7 +22,31 @@ module sdram(
   wire [2:0] CL = mode_reg[6:4];
   wire [2:0] BL = mode_reg[2:0];
 
-  localparam IDLE = 2'd0, READ = 2'd1, WAIT = 2'd2;
+  always @(posedge clk) begin
+    if(cke) begin
+      case(cmd)
+        4'b0000: begin // LOAD_MODE
+          mode_reg <= a;
+        end
+        4'b0011: begin // ACTIVE
+          row_addr[ba] <= a;
+        end
+        4'b0101: begin // READ
+          col_addr <= a[8:0];
+          bank_sel <= ba;
+        end
+        4'b0100: begin // WRITE
+          col_addr <= a[8:0];
+          bank_sel <= ba;
+        end
+        default: begin
+          // nop
+        end
+      endcase
+    end
+  end
+
+  localparam IDLE = 2'd0, READ = 2'd1, WAIT = 2'd2, WRITE = 2'd3;
   reg [1:0] state, next_state;
 
   reg [2:0] burst_cnt;
@@ -36,10 +60,22 @@ module sdram(
     end
   end
 
+  reg [2:0] write_cnt;
+  always @(posedge clk) begin
+    if(cke) begin
+      if((state == IDLE && cmd == 4'b0100) || state == WRITE) begin
+        write_cnt <= write_cnt + 3'd1;
+      end
+      else begin
+        write_cnt <= 0;
+      end
+    end
+  end
+
   always @(*) begin
     case(state)
       IDLE: begin
-        next_state = (cmd == 4'b0101) ? (CL == 3'd2 ? READ : WAIT) : IDLE;
+        next_state = (cmd == 4'b0101) ? (CL == 3'd2 ? READ : WAIT) : (cmd == 4'b0100 ? WRITE : IDLE);
       end
 
       READ: begin
@@ -72,31 +108,16 @@ module sdram(
     end
   end
 
-  wire o_en = (state == READ) && (burst_cnt > 0);
-
+  wire [8:0] write_burst_addr = col_addr + {6'b0, write_cnt};
   always @(posedge clk) begin
     if(cke) begin
-      case(cmd)
-        4'b0000: begin // LOAD_MODE
-          mode_reg <= a;
-        end
-        4'b0011: begin // ACTIVE
-          row_addr[ba] <= a;
-        end
-        4'b0101: begin // READ
-          col_addr <= a[8:0];
-          bank_sel <= ba;
-        end
-        4'b0100: begin // WRITE
-          sdram_write({6'b0, ba}, {19'b0, row_addr[ba]}, {23'b0, col_addr[8:0]}, dq, {6'b0, dqm});
-        end
-        default: begin
-          // nop
-        end
-      endcase
+      if(state == WRITE && write_cnt > 0 && write_cnt <= BL) begin
+        sdram_write({6'b0, bank_sel}, {19'b0, row_addr[bank_sel]}, {23'b0, write_burst_addr}, dq, {6'b0, dqm});
+      end
     end
   end
 
+  wire o_en = (state == READ) && (burst_cnt > 0);
 
   assign dq = o_en ? read_data : 16'bz;
 
