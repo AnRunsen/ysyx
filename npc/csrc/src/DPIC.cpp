55091dbb7c37fbd2ff81f4854876f34578099b99
tracer-ysyx <tracer@ysyx.org>
2026-05-05 20:58:12 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:58:12 up  8:52,  5 users,  load average: 0.40, 0.61, 0.57

--- npc/csrc/src/DPIC.cpp
+++ npc/csrc/src/DPIC.cpp
@@ -141,7 +141,6 @@ uint32_t sdram_read_laddr(uint32_t addr){
 }
 
 void sdram_write_laddr(uint32_t addr, uint16_t data) {
-    printf("sdram write: addr=0x%08x, data=0x%04x\n", addr, data);
     addr &= ~0x3u;
 
     uint32_t row_addr = (addr >> 12) & 0x1fffu;
@@ -149,6 +148,7 @@ void sdram_write_laddr(uint32_t addr, uint16_t data) {
     uint32_t col_addr = ((addr >> 2) & 0xffu) << 1;
 
     sdram[bank][row_addr][col_addr] = data;
+    printf("sdram write: bank=%d, row_addr=0x%08x, col_addr=0x%08x, data=0x%04x\n", bank, row_addr, col_addr, data);
 }
 
 
