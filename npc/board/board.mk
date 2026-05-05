5fd09482a75464081a711bdb8863791d301ea415
tracer-ysyx <tracer@ysyx.org>
2026-05-05 13:58:50 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:58:50 up  1:53,  4 users,  load average: 0.79, 0.50, 0.49

--- npc/board/board.mk
+++
@@ -1,8 +0,0 @@
-include $(NVBOARD_HOME)/scripts/nvboard.mk
-BIND_FILE = $(abspath ./board/auto_bind.cpp)
-LDFLAGS += $(NVBOARD_ARCHIVE) -lSDL2 -lSDL2_image -lSDL2_ttf
-CFLAGS += -I$(abspath $(NVBOARD_HOME)/usr/include)
-XDC_FILE = $(abspath ./board/myxdc.nxdc)
-
-$(BIND_FILE): $(XDC_FILE)
-	python3 $(NVBOARD_HOME)/scripts/auto_pin_bind.py $^ $@
