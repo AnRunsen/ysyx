8b651c22cf14e301708dea284b01ef85d182a2d2
tracer-ysyx <tracer@ysyx.org>
2026-05-08 13:12:28 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:12:28 up 44 min,  3 users,  load average: 2.26, 2.18, 1.71

--- npc/csrc/src/DPIC.cpp
+++ npc/csrc/src/DPIC.cpp
@@ -132,9 +132,9 @@ extern "C" void uart_write(int waddr, int wdata, uint8_t wmask)
 
 extern "C" void sim_exit(int code)
 {
-    printf("\033[34mTotal Cycles: \t%lu\n", cycle_cnt);
+    printf("\033[34mTotal Cycles: \t\t%lu\n", cycle_cnt);
     printf("Total Instructions: \t%lu\n", instr_cnt);
-    printf("IPC: \t%f\n", (double)instr_cnt / (double)cycle_cnt);
+    printf("IPC: \t\t\t%f\n", (double)instr_cnt / (double)cycle_cnt);
 
     printf("Performance Counters:\n");
     printf("  IFU: %lu\n", perf_cnt.ifu);
