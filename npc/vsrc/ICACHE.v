7497edfa4487bf99a38ad582f942e5e31669d5b6
tracer-ysyx <tracer@ysyx.org>
2026-05-21 17:38:41 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:38:41 up  4:49,  7 users,  load average: 0.81, 0.61, 0.64

--- npc/vsrc/ICACHE.v
+++ npc/vsrc/ICACHE.v
@@ -149,15 +149,18 @@ module ICACHE#(
     /*logic to latch data*/
     reg [3:0] id_reg;
     reg [31:0] addr_reg;
+    reg [$clog2(LINE_NUM)-1:0] index_reg;
     wire [WORDS_SEL_SIZE-1:0] word_sel = addr_reg[$clog2(LINE_SIZE)-1:2];
     always @(posedge clk) begin
         if(reset) begin
             id_reg <= 4'b0;
             addr_reg <= 32'b0;
+            index_reg <= 0;
         end
         else if(state == IDLE && s_axi_arvalid && s_axi_arready) begin
             id_reg <= s_axi_arid;
             addr_reg <= s_axi_araddr;
+            index_reg <= index;
         end
     end
 
@@ -175,9 +178,9 @@ module ICACHE#(
             recv_counter <= 4'b0;
         end
         else if(state == WAIT && m_axi_rvalid && m_axi_rready) begin
-            cache[index][recv_counter*32 +: 32] <= m_axi_rdata;
-            tags[index] <= tag;
-            valid[index] <= 1'b1;
+            cache[index_reg][recv_counter*32 +: 32] <= m_axi_rdata;
+            tags[index_reg] <= tag;
+            valid[index_reg] <= 1'b1;
             if(m_axi_rlast) begin
                 recv_counter <= 4'b0;
             end
@@ -189,7 +192,7 @@ module ICACHE#(
 
     assign s_axi_arready = state == IDLE;
     assign s_axi_rid = id_reg;
-    assign s_axi_rdata = cache[index][word_sel*32 +: 32];
+    assign s_axi_rdata = cache[index_reg][word_sel*32 +: 32];
     assign s_axi_rresp = 2'b00;
     assign s_axi_rlast = 1'b1;
     assign s_axi_rvalid = state == RESP;
