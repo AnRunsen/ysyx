e05321bc0857f2348fcac5a29dc7a69ac760f595
tracer-ysyx <tracer@ysyx.org>
2026-05-24 17:52:34 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:52:34 up  2:11,  6 users,  load average: 1.30, 0.87, 0.72

--- npc/csrc/include/config.hpp
+++ npc/csrc/include/config.hpp
@@ -1,11 +1,11 @@
 #ifndef __CONFIG_HPP__
 #define __CONFIG_HPP__
 
-// #define ITRACE
+#define ITRACE
 // #define FTRACE
 // #define DIFFTEST
 // #define MTRACE
-#define WAVEON
+// #define WAVEON
 // #define PIPELINE_VIEW
 
 #endif
