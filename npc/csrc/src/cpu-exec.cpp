547abf1e5d2a76adaaf9f499037407dcc0438580
tracer-ysyx <tracer@ysyx.org>
2026-05-26 18:34:31 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:34:31 up  5:36,  6 users,  load average: 0.79, 0.59, 0.66

--- npc/csrc/src/cpu-exec.cpp
+++ npc/csrc/src/cpu-exec.cpp
@@ -56,13 +56,14 @@ void cpu_exec(uint64_t n)
             uint8_t exu_valid = cpu->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__ysyx_26040125_EXU__DOT__valid_reg;
             uint8_t lsu_valid = cpu->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__ysyx_26040125_LSU__DOT__valid_reg;
             uint8_t wbu_valid = cpu->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__ysyx_26040125_WBU__DOT__valid_reg;
-
-            printf("\r| %s | %s | %s | %s | %s |",
+            uint32_t pc = cpu->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__PCR_PC;
+            printf("\r| %s | %s | %s | %s | %s |     PC: 0x%08x |",
                 ifu_valid ? "IFU" : "   ",
                 idu_valid ? "IDU" : "   ",
                 exu_valid ? "EXU" : "   ",
                 lsu_valid ? "LSU" : "   ",
-                wbu_valid ? "WBU" : "   ");
+                wbu_valid ? "WBU" : "   ",
+                pc);
             fflush(stdout);
 #endif
         }
