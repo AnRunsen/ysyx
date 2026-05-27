5a470b987feb0e2337d5fae3534a3801428174c6
tracer-ysyx <tracer@ysyx.org>
2026-05-27 16:32:16 +0800

    > sim RTL ysyx_26040125 安润森 Linux Legion 6.8.0-117-generic #117~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu May  7 22:17:46 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:32:16 up  6:34,  6 users,  load average: 1.10, 0.75, 0.45

--- npc/csrc/src/CPU.cpp
+++ npc/csrc/src/CPU.cpp
@@ -19,6 +19,7 @@ uint64_t cycle_cnt = 0;
 uint64_t instr_cnt = 0;
 uint64_t ihit_cnt = 0;
 uint64_t ifetch_cnt = 0;
+uint64_t flush_cnt = 0;
 bool bootloader_stage = true;
 
 bool exit_flag = false;
